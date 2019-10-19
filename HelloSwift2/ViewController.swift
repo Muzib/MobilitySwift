d5bd3b5b0789fd26576a746d4ac3b1edf0eaf449
Muzib Ahamed <muzib.a@gmail.com>
2019-10-18 20:46:57 -0500

    Initial commit

---
+++ HelloSwift2/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  HelloSwift2
+//
+//  Created by Muzib Ahamed Shaik on 10/18/19.
+//  Copyright © 2019 Muzib Ahamed Shaik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
